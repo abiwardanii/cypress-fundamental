41e5cbe29f76e0984c97f3e643d4a2448db43924
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-08-23 13:22:01 +0700

    publish-form

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -20,4 +20,28 @@ describe('Dashboard Page Test Case', () => {
     it('Found no post for the first time', () => {
         cy.contains("Found 0 photos")
     });
+
+    //Publish form
+    it('Contains Image url and description input and Publish button', () => {
+        //check image
+        const image = cy.get("input[name='image']");
+        image.should('be.visible');
+        image.should('have.attr','type','url');
+        image.should('have.attr','required','required');
+        image.should('have.attr','placeholder','Image URL');
+
+        //check description
+        const description = cy.get("input[name='desc']");
+        description.should('be.visible');
+        description.should('have.attr','type','text');
+        description.should('have.attr','required','required');
+        description.should('have.attr','placeholder',"What's on your mind?");
+
+        //check publish button
+        const button = cy.get('button');
+        button.should("be.visible");
+        button.contains("Publish!");
+        button.should("have.css","background-color","rgb(79, 70, 229)");
+        button.should("have.css","color","rgb(255, 255, 255)");
+    });
  })
